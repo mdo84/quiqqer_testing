8f2f2c8f9b190acb3031a748a2fb2c0e51d3a00b
markus <markus@mdo.fedora>
2015-10-18 22:56:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# quiqqer\_testing
+
+Dieses Script erzeugt zwei Dockercontainer für quiqqer.
+- ein Container mit PHP5 und Apache2 (2.4)
+- ein Container mit Mysql 5.5.x
+
+##Usage
+sudo bash quiqqer.sh <OPTION>
+ start <GITREPO>     - start the containers mysql & apache2 with quiqqer installed
+ build <GITREPO>     - building apache2 container, download quiqqer from repo
+ stop\_all\_containers - stopping and deleting ALL containers on the host
+
+##Abhängigkeiten
+- bash
+- docker
+- git
+
+##Zugangsdaten Datenbank
+###mysql
+host:    mysqldb
+root-pw: quiqqer
+db:      quiqqer
+
+##Testen der Container
+"localhost" im Browser eingeben
+
---
+++ quiqqer.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# using docker to test quiqqer
+#
+# Usage:
+#  start               - start the containers mysql & apache2 with quiqqer installed"
+#  build               - building apache2 container, download quiqqer from repo"
+#  stop_all_containers - stopping and deleting ALL containers on the host"
+#
+#
+# check if docker is installed
+if [[ ! -e /bin/docker ]]
+then
+  echo "Docker is not installed"
+  exit 1
+fi
+
+OPTION=${1}
+ENVGIT=${2}
+DOCKERFILEPATH=Dockerfiles/
+CONTAINER=apache2
+
+function build {
+  echo "Using ${ENVGIT}"
+  sed "s#ENVGIT#${ENVGIT}#g" ${DOCKERFILEPATH}${CONTAINER}/Dockerfile.template > ${DOCKERFILEPATH}${CONTAINER}/Dockerfile
+  echo "Using the following Dockerfile"
+  cat ${DOCKERFILEPATH}${CONTAINER}/Dockerfile
+  docker build -t quiqqer/apache2 ${DOCKERFILEPATH}${CONTAINER}
+  echo "Cleanup Dockerfile..."
+  rm -f Dockerfiles/apache2/Dockerfile
+}
+
+function start {
+
+  # downloading mysqldb from dockerhub
+  docker run --name mysqldb -e MYSQL_ROOT_PASSWORD=quiqqer -e MYSQL_DATABASE=quiqqer -d mysql
+
+  # use the built image
+  docker run --name apache2 --link mysqldb:mysql -p 80:80 -d quiqqer/apache2 /usr/sbin/apache2ctl -D FOREGROUND
+
+}
+
+function stop_all_containers {
+
+  echo "Stopping all Containers...."
+  # stopping all docker container
+  docker stop $(docker ps -a -q)
+
+  echo "Removing all Containers..."
+  # remove als docker container
+  docker rm --volumes=true  $(docker ps -a -q)
+
+}
+
+function repo_check {
+  # check if EVNGIT is set
+  if [[ -z ${ENVGIT} ]]; then
+    echo "GITREPO is missing..."
+    usage
+    exit 1
+  fi
+
+}
+
+function usage {
+
+  echo Usage:
+  echo " start <GITREPO>     - start the containers mysql & apache2 with quiqqer installed"
+  echo " build <GITREPO>     - building apache2 container, download quiqqer from repo"
+  echo " stop_all_containers - stopping and deleting ALL containers on the host"
+
+}
+
+case ${OPTION} in
+start)
+  repo_check
+  build
+  start
+  ;;
+stop_all_containers)
+  stop_all_containers
+  ;;
+build)
+  repo_check
+  build
+  ;;
+*)
+  usage
+  ;;
+esac
+
